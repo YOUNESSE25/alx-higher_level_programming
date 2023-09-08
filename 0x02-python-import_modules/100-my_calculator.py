acdc1ac992b6f9f9f909dfa2db3bc74df11447c6
YOUNESSE25 <younessehaouas@gmail.com>
2023-09-08 02:03:03 -0700

    python

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -20,5 +20,4 @@ else:
     print("Unknown operator. Available operators: +, -, * and /")
     exit(1)
 
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2],
-    int(argv[3]), op(int(argv[1]), int(argv[3]))))
+print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2], int(argv[3]), op(int(argv[1]), int(argv[3]))))
