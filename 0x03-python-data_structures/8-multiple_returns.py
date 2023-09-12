388780ae46eab2381deeab3f181f6a2216522fde
YOUNESSE25 <younessehaouas@gmail.com>
2023-09-11 18:00:02 -0700

    python

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    longg = len(sentence)
-    if longg == 0:
+    l = len(sentence)
+    if l == 0:
         r = (0, None)
         return r
     else:
-        r = (longg, sentence[0:1])
+        r = (l, sentence[0:1])
         return r
