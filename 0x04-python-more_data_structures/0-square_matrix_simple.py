373252e1077e1bcf114a941ae868dc049490f946
YOUNESSE25 <younessehaouas@gmail.com>
2023-09-13 04:21:22 -0700

    python

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def sqr(x):
+    return (x**2)
+def square_matrix_simple(matrix=[]):
+    n = matrix[:]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            n[i] = list(map(sqr, matrix[i]))
+    return n
