4bb4fe633188be196af9bf24766a4e92212d1287
YOUNESSE25 <younessehaouas@gmail.com>
2023-09-26 17:52:10 -0700

    Python - Classes and Objects

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -3,6 +3,7 @@
 
 
 class Square:
+    """class Square that defines a square by: (based on 5-square.py)"""
     def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
