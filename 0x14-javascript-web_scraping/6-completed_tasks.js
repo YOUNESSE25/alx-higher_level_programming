d6809959906f199cf3d90f7a8c178a823af2444b
YOUNESSE25 <younessehaouas@gmail.com>
2024-03-27 00:39:06 +0000

    javascript-web_scraping

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -3,14 +3,14 @@ const req = require('request');
 req(process.argv[2], function (err, response, body) {
   if (!err) {
     const TODOS = JSON.parse(body);
-    const FINISH = {};
+    const completed = {};
     TODOS.forEach((TODO) => {
-      if (TODO.FINISH && FINISH[TODO.userId] === undefined) {
-        FINISH[TODO.userId] = 1;
-      } else if (TODO.FINISH) {
-        FINISH[TODO.userId] += 1;
+      if (TODO.completed && completed[TODO.userId] === undefined) {
+        completed[TODO.userId] = 1;
+      } else if (TODO.completed) {
+        completed[TODO.userId] += 1;
       }
     });
-    console.log(FINISH);
+    console.log(completed);
   }
 });
