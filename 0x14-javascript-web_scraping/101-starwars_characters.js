b69a24acc4226499ebe2dfc6ad8670526658efdd
YOUNESSE25 <younessehaouas@gmail.com>
2024-03-27 01:29:53 +0000

    javascript-web_scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -4,15 +4,15 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 req(url, function (err, response, body) {
   if (!err) {
     const characters = JSON.parse(body).characters;
-    Characters_print(characters, 0);
+    printCharacters(characters, 0);
   }
 });
-function Characters_print (characters, index) {
+function printCharacters (characters, index) {
   req(characters[index], function (error, response, body) {
     if (!error) {
       console.log(JSON.parse(body).name);
       if (index + 1 < characters.length) {
-        Characters_print(characters, index + 1);
+        printCharacters(characters, index + 1);
       }
     }
   });
